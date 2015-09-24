31ce234a7d296ee0d69ab1db36d3df96327ad624
Will Schive <lexigram@goodworksonearth.org>
2015-09-23 21:06:38 -0700

    prepare_error_examples tests pass

--- spec/integration_examples/prepare_error_examples.rb
+++ spec/integration_examples/prepare_error_examples.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
-
 shared_examples 'Card Error Prep' do
-
-  it 'prepares a card error' do
+  it 'prepares a card error', live: true do
+    StripeMock.start
     StripeMock.prepare_card_error(:card_declined, :new_charge)
     cus = Stripe::Customer.create(email: 'alice@example.com')
     expect { Stripe::Charge.create(
@@ -14,23 +13,23 @@ shared_examples 'Card Error Prep' do
     }.to raise_error(Stripe::CardError, 'The card was declined')
   end
 
-  it 'is a valid card error' do
-    stripe_helper.prepare_card_error
-
+  it 'is a valid card error', live: true do
     begin
+      StripeMock.start
+      StripeMock.prepare_card_error(:card_declined)
       Stripe::Customer.create(
-        email: 'alice@bob.com',
+        email: 'alice@example.com',
         source: stripe_helper.generate_card_token(number: '123')
       )
     rescue Stripe::CardError => e
       body = e.json_body
       err  = body[:error]
-
       expect(body).to be_a(Hash)
       expect(err[:type]).to eq 'card_error'
       expect(err[:param]).to eq 'number'
       expect(err[:code]).to eq 'invalid_number'
       expect(err[:message]).to eq 'This card number looks invalid.'
+      StripeMock.stop
     end
   end
 end
